d16472a65bdca8cc62888984fb2be8ea8a10b4e2
Grzegorz Możdżeń <grzegorz.mozdzen@oxm.pl>
2022-02-17 07:57:16 +0100

    Fix after login page

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use TCG\Voyager\Facades\Voyager;
+use TCG\Voyager\Http\Controllers\VoyagerAuthController;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(VoyagerAuthController::class, \App\Http\Controllers\VoyagerAuthController::class);
     }
 
     /**

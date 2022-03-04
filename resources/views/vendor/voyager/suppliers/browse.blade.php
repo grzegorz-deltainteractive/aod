ac28fff8aac6e42d85ab1d0e7964b98f4a43a7b2
pawell <p.lipinski@deltainteractive.pl>
2022-03-04 08:34:27 +0100

    Commit views suppliers

--- resources/views/vendor/voyager/suppliers/browse.blade.php
+++ resources/views/vendor/voyager/suppliers/browse.blade.php
@@ -38,8 +38,7 @@
         @endforeach
         @include('voyager::multilingual.language-selector')
 
-
-		<div style="display:none; margin-top:-102px; background:#fff; right:420px; position:fixed; border:1px solid green;  index-z:9999; width:400px; padding:15px;  -webkit-border-radius: 3px;
+		<div style="margin-top:-102px; background:#fff; right:420px; position:fixed; border:1px solid green;  index-z:9999; width:400px; padding:15px;  -webkit-border-radius: 3px;
 -moz-border-radius: 3px;
 border-radius: 3px; text-align:center;">Plik został wczytany.<br/>Dostawcy którzy zostali pominięci:<br/>-</div>
 		<script>function sprawdz(){ var h=document.getElementById('plik').value; if(h==""){ document.getElementById('plik').style.border="1px solid red"; return false; } }</script>
